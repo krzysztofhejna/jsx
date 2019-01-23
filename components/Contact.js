4700db16601945ba49911d986881d9849ad4653e
krzysztofhejna <khejna15@wp.pl>
2019-01-23 19:56:55 +0100

    Update app with jsx

--- components/Contact.js
+++ components/Contact.js
@@ -14,15 +14,6 @@ var Contact = React.createClass({
           {this.props.item.email}
         </a>
       </div>
-
-
-      // React.createElement('div', {className: 'contactItem'},
-      //   React.createElement('i', {
-      //     className: 'fas fa-user',
-      //   }),
-      //   React.createElement('p', {className: 'contactLabel'}, this.props.item.firstName + ' ' + this.props.item.lastName),
-      //   React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email}, this.props.item.email),
-      // )
     )
   },
 });
